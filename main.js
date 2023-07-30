127cc0c1e100ba65b8b47e1daa36e0bf4bb1edd9
BarnoTuranova <138581787+BarnoTuranova@users.noreply.github.com>
2023-07-30 17:01:15 +0500

    Javascript 5-dars
    24-guruh o'quvchisi

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector(".button").addEventListener('copy', function(event){
+	console.log("Diqqat! Diqqat! Sizdan "+event.target.value+ "  Nomli hujjat ko'chirildi")
+})
+document.querySelector(".button1").addEventListener('update', function(event){
+	console.log("Diqqat! Diqqat! Sizdan "+event.target.value+ "  Nomli hujjat tasdiqlandi")
+})
+
+
